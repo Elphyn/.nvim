2e7811935d714f8e3acdf6ca62e1328ef105de6a
vlad <elphynkarelov@gmail.com>
2025-12-17 22:25:23 +0900

    GIT: added neogit plugin

---
+++ lua/plugins/git.lua
@@ -0,0 +1,15 @@
+return {
+  "NeogitOrg/neogit",
+  lazy = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+
+    -- Only one of these is needed.
+    "folke/snacks.nvim",
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+  }
+}
